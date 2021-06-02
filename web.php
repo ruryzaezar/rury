60673b602e8d93ad5f78ac33bc405ef37191c999
ruryzaezar <85207671+ruryzaezar@users.noreply.github.com>
2021-06-02 10:07:15 +0700

    Add files via upload

---
+++ web.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Toko Kue RM Sari</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	
+	<header>
+		<h1 class="judul">Toko Kue RM Sari Cake</h1>
+		<p class="deskripsi">Menjual Berbagai Macam Aneka Kue kering & Basah</p>
+	</header>
+	
+	<div class="wrap">
+		<nav class="menu">
+			<ul>
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">About</a>
+				</li>
+				<li>
+					<a href="#">Contact</a>
+				</li>
+			</ul>
+		</nav>
+
+		<aside class="sidebar">
+			<div class="widget">
+				<h2>Toko Kami</h2>
+				<p>Selamat datang di Toko Kue RM Sari Cake</p>
+			</div>
+		</aside>
+ 
+		<div class="blog">
+			<div class="conteudo">
+				<div class="post-info">
+					Di Posting Oleh <b>Toko RM Sari</b>
+				</div>
+				<h1>Produk</h1>
+				<img src="kue.jpg" height="300px" width="100px;">
+				<hr>				
+				<a href="#" class="continue-lendo">Selengkapnya →</a>
+			</div>
+			<div class="conteudo">
+				<div class="post-info">
+					Di Posting Oleh <b>Toko RM Sari</b>
+				</div>
+				<h1>Usaha Kami</h1>
+				<img src="kuwe.jpg" height="300px" width="100px;">
+				<hr>
+				<p>Menerima Pesanan Bolu ultah, Pernikahan, Khitanan dll</p>				
+				<a href="#" class="continue-lendo">Selengkapnya →</a>
+			</div>
+		</div>
+
+	</div>
+ 
+</body>
+</html>
